bc11bd196d7251fde3f85e427094f79ba645076a
nguyenminhthong2020 <thanhle.minhthong1@gmail.com>
2025-03-01 12:17:22 +0700

    up

--- src/main/java/com/example/Job/config/MapperConfig.java
+++ src/main/java/com/example/Job/config/MapperConfig.java
@@ -9,9 +9,8 @@ import org.springframework.context.annotation.Configuration;
 public class MapperConfig {
 
     @Bean
-    public ModelMapper modelMapper(){
+    public ModelMapper modelMapper() {
         return new ModelMapper();
     }
-
 
 }
